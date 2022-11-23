57aa40fdc39a8319c45788e1d6d28f0b1101fadf
Upal Barua <upalbarua52@gmail.com>
2022-11-23 20:30:50 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('server running...');
+});
+
+app.listen(port);
