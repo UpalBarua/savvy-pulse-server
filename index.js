293c8e8e0b23df9bca952fb5dd6224c6d670c4f4
Upal Barua <upalbarua52@gmail.com>
2022-11-24 13:11:10 +0600

    db setup and categories api added

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('server running...');
+  res.send('SavvyPulse server running...');
 });
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4w0vbzl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const categoriesCollection = client
+      .db('savvy-pulse')
+      .collection('categories');
+
+    app.get('/categories', async (req, res) => {
+      const response = await categoriesCollection.find({}).toArray();
+      res.json(response);
+    });
+  } finally {
+  }
+};
+
+run().catch((error) => console.log(error));
 
 app.listen(port);
